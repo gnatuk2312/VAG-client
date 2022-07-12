e49bc8c3c29449801bffffac989330e479e77aea
gnatuk2312 <hnat.liashenko@bluepes.com>
2022-07-12 23:30:19 +0300

    VAG-40: create drawer component

---
+++ components/drawer.jsx
@@ -0,0 +1,104 @@
+import Link from "next/link";
+import cn from "classnames";
+
+import Container from "./container";
+
+const Drawer = (props) => {
+  const { isOpen, onClose } = props;
+
+  return (
+    <div className={cn("drawer", { "drawer_is-open": isOpen })}>
+      <Container className="drawer__container">
+        <ul className="drawer__links">
+          <li onClick={onClose} className="drawer__link-item">
+            <Link href="/" passHref>
+              <a href="replace">Про нас</a>
+            </Link>
+          </li>
+          <li onClick={onClose} className="drawer__link-item">
+            <Link href="/" passHref>
+              <a href="replace">Послуги</a>
+            </Link>
+          </li>
+          <li onClick={onClose} className="drawer__link-item">
+            <Link href="/" passHref>
+              <a href="replace">Записатись</a>
+            </Link>
+          </li>
+          <li onClick={onClose} className="drawer__link-item">
+            <Link href="/" passHref>
+              <a href="replace">Контакти</a>
+            </Link>
+          </li>
+        </ul>
+        <ul className="drawer__main-info">
+          <li className="drawer__main-info-item">
+            <span>S:</span>
+            <ul>
+              <li className="drawer__social-item">
+                <a
+                  href="https://www.instagram.com/slavek_grabovskiy"
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={onClose}
+                >
+                  Instagram
+                </a>
+              </li>
+              <li className="drawer__social-item">
+                <a
+                  href="https://uk-ua.facebook.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={onClose}
+                >
+                  Facebook
+                </a>
+              </li>
+              <li className="drawer__social-item">
+                <a
+                  href="https://www.youtube.com/channel/UCfO-su5F0y7n1ew39LgmoGg"
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={onClose}
+                >
+                  YouTube
+                </a>
+              </li>
+              <li className="drawer__social-item">
+                <a
+                  href="https://vm.tiktok.com/ZMNPKwj98"
+                  target="_blank"
+                  rel="noreferrer"
+                  onClick={onClose}
+                >
+                  TikTok
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="drawer__main-info-item">
+            <span>L:</span>
+            <address>
+              Тернопіль, <br />
+              вул. Бродівська 59
+            </address>
+          </li>
+          <li className="drawer__main-info-item">
+            <span>C:</span>
+            <ul>
+              <li className="drawer__contacts-item">
+                <a href="tel:+380679992655">+38 067 999 26 55</a>
+              </li>
+              <li className="drawer__contacts-item">
+                <a href="mailto:vagautotern@gmail.com">vagautotern@gmail.com</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </Container>
+    </div>
+  );
+};
+
+export default Drawer;
--- components/navigation.jsx
+++ components/navigation.jsx
@@ -5,57 +5,63 @@ import Link from "next/link";
 import Container from "./container";
 import Logo from "./logo";
 import useVerticalScrollProps from "../hooks/useVerticalScrollProps";
+import Drawer from "./drawer";
 
 const Navigation = () => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
   const handleDrawerState = () => setDrawerIsOpen((prev) => !prev);
 
+  const handleDrawerClose = () => setDrawerIsOpen(false);
+
   const { isScrollUp, isScrolled } = useVerticalScrollProps();
 
   return (
-    <nav
-      className={cn(
-        "navigation",
-        { "navigation_is-drawer-open": drawerIsOpen },
-        { "navigation_is-visible": isScrollUp || !isScrolled },
-      )}
-    >
-      <Container className="navigation__container">
-        <div className="navigation__logo">
-          <Logo />
-          <p>
-            автоелектрик <br />
-            <span>vag group</span>
-          </p>
-        </div>
-        <ul className="navigation__links">
-          <li className="navigation__item">
-            <Link href="/" passHref>
-              <a href="replace">Про нас</a>
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link href="/" passHref>
-              <a href="replace">Послуги</a>
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link href="/" passHref>
-              <a href="replace">Записатись</a>
-            </Link>
-          </li>
-          <li className="navigation__item">
-            <Link href="/" passHref>
-              <a href="replace">Контакти</a>
-            </Link>
-          </li>
-        </ul>
-        <button type="button" onClick={handleDrawerState} className="navigation__burger-menu">
-          <span />
-        </button>
-      </Container>
-    </nav>
+    <>
+      <nav
+        className={cn(
+          "navigation",
+          { "navigation_is-drawer-open": drawerIsOpen },
+          { "navigation_is-visible": isScrollUp || !isScrolled },
+        )}
+      >
+        <Container className="navigation__container">
+          <div className="navigation__logo">
+            <Logo />
+            <p>
+              автоелектрик <br />
+              <span>vag group</span>
+            </p>
+          </div>
+          <ul className="navigation__links">
+            <li className="navigation__item">
+              <Link href="/" passHref>
+                <a href="replace">Про нас</a>
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <Link href="/" passHref>
+                <a href="replace">Послуги</a>
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <Link href="/" passHref>
+                <a href="replace">Записатись</a>
+              </Link>
+            </li>
+            <li className="navigation__item">
+              <Link href="/" passHref>
+                <a href="replace">Контакти</a>
+              </Link>
+            </li>
+          </ul>
+          <button type="button" onClick={handleDrawerState} className="navigation__burger-menu">
+            <span />
+          </button>
+        </Container>
+      </nav>
+      <Drawer isOpen={drawerIsOpen} onClose={handleDrawerClose} />
+    </>
   );
 };
 
