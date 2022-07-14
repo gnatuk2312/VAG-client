d988a126701276fa1bf80bf4e5c2bdd7125b2b64
gnatuk2312 <hnat.liashenko@bluepes.com>
2022-07-14 18:05:45 +0300

    VAG-45: create text-carousel section

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "../sections/header";
 import AboutCompany from "../sections/about-company";
 import Services from "../sections/services";
 import CarCarousel from "../sections/car-carousel";
+import TextCarousel from "../sections/text-carousel";
 
 const Home = () => (
 	<>
@@ -11,6 +12,7 @@ const Home = () => (
 		<AboutCompany />
 		<Services />
 		<CarCarousel />
+		<TextCarousel />
 		<div className="normal">Славчик - Маркетолог</div>
 		<div className="light">Проект = Автоелектрик</div>
 		<div className="ultra">Гнатик - Програміст</div>
---
+++ sections/text-carousel.jsx
@@ -0,0 +1,25 @@
+import Container from "../components/container";
+
+const TextCarousel = () => (
+  <section className="text-carousel">
+    <Container className="text-carousel__container">
+      <p className="text-carousel__text">
+        Тернопіль автоелектрик
+        <span className="text-carousel__text_green"> vag group Тернопіль</span> автоелектрик
+        <span className="text-carousel__text_green"> vag group</span>
+      </p>
+      <p className="text-carousel__text">
+        Тернопіль автоелектрик
+        <span className="text-carousel__text_green"> vag group Тернопіль</span> автоелектрик
+        <span className="text-carousel__text_green"> vag group</span>
+      </p>
+      <p className="text-carousel__text">
+        Тернопіль автоелектрик
+        <span className="text-carousel__text_green"> vag group Тернопіль</span> автоелектрик
+        <span className="text-carousel__text_green"> vag group</span>
+      </p>
+    </Container>
+  </section>
+);
+
+export default TextCarousel;
