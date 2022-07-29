4ac0161863c491350c93af4b3ae9d45188c30994
gnatuk2312 <hnat.liashenko@bluepes.com>
2022-07-29 16:47:09 +0300

    VAG-53: create admin-drawer component

---
+++ components/admin/admin-drawer.jsx
@@ -0,0 +1,50 @@
+import cn from "classnames";
+import Link from "next/link";
+
+import HomeIcon from "../../public/icons/home-icon.svg";
+import AddIcon from "../../public/icons/add-icon.svg";
+import ClientsIcon from "../../public/icons/clients-icon.svg";
+import ExitIcon from "../../public/icons/exit-icon.svg";
+
+const AdminDrawer = (props) => {
+  const { isOpen, onClose } = props;
+
+  return (
+    <section className={cn("admin-drawer", { "admin-drawer_is-open": isOpen })}>
+      <div className="admin-drawer__container">
+        <ul>
+          <li className="admin-drawer__link-item">
+            <Link href="/admin" passHref>
+              <a onClick={onClose} className="admin-drawer__link" href="replace">
+                <HomeIcon />
+                Головна
+              </a>
+            </Link>
+          </li>
+          <li className="admin-drawer__link-item">
+            <Link href="/admin" passHref>
+              <a onClick={onClose} className="admin-drawer__link" href="replace">
+                <AddIcon />
+                Записати
+              </a>
+            </Link>
+          </li>
+          <li className="admin-drawer__link-item">
+            <Link href="/admin" passHref>
+              <a onClick={onClose} className="admin-drawer__link" href="replace">
+                <ClientsIcon />
+                Клієнти
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <button type="button" className="admin-drawer__exit">
+          <ExitIcon />
+          Вийти
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default AdminDrawer;
--- components/admin/admin-navigation.jsx
+++ components/admin/admin-navigation.jsx
@@ -3,6 +3,7 @@ import cn from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
+import AdminDrawer from "./admin-drawer";
 import HomeIcon from "../../public/icons/home-icon.svg";
 import AddIcon from "../../public/icons/add-icon.svg";
 import ClientsIcon from "../../public/icons/clients-icon.svg";
@@ -14,74 +15,82 @@ const AdminNavigation = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const handleDrawer = () => setIsDrawerOpen((prev) => !prev);
+  const handleDrawerClose = () => setIsDrawerOpen(false);
 
   return (
-    <div className="admin-navigation">
-      <div className="admin-navigation__mobile">
-        <p className="admin-navigation__logo">
-          АВТОЕЛЕКТРИК <br /> <span className="admin-navigation__logo_green">VAG GROUP</span>
-        </p>
-        <button
-          type="button"
-          onClick={handleDrawer}
-          className={cn("admin-navigation__burger-menu", {
-            "admin-navigation__burger-menu_is-drawer-open": isDrawerOpen,
-          })}
-        >
-          <span />
-        </button>
-      </div>
-      <sidebar className="admin-navigation__sidebar">
-        <div className="admin-navigation__sidebar-wrapper">
-          <p className="admin-navigation__logo">
-            АВТОЕЛЕКТРИК <br /> <span className="admin-navigation__logo_green">VAG GROUP</span>
-          </p>
-          <ul>
-            <li
-              className={cn("admin-navigation__link-item", {
-                "admin-navigation__link-item_active": asPath === "/admin",
-              })}
-            >
-              <Link href="/admin" passHref>
-                <a className="admin-navigation__link" href="replace">
-                  <HomeIcon />
-                  Головна
-                </a>
-              </Link>
-            </li>
-            <li
-              className={cn("admin-navigation__link-item", {
-                "admin-navigation__link-item_active": asPath === "/admin",
-              })}
-            >
-              <Link href="/admin" passHref>
-                <a className="admin-navigation__link" href="replace">
-                  <AddIcon />
-                  Записати
-                  <span>2</span>
-                </a>
-              </Link>
-            </li>
-            <li
-              className={cn("admin-navigation__link-item", {
-                "admin-navigation__link-item_active": asPath === "/admin",
-              })}
-            >
-              <Link href="/admin" passHref>
-                <a className="admin-navigation__link" href="replace">
-                  <ClientsIcon />
-                  Клієнти
-                </a>
-              </Link>
-            </li>
-          </ul>
-          <button type="button" className="admin-navigation__exit">
-            <ExitIcon />
-            Вийти
+    <>
+      <nav className="admin-navigation">
+        <div className="admin-navigation__mobile">
+          <Link href="/admin" passHref>
+            <a onClick={handleDrawerClose} className="admin-navigation__logo" href="replace">
+              АВТОЕЛЕКТРИК <br /> <span className="admin-navigation__logo_green">VAG GROUP</span>
+            </a>
+          </Link>
+          <button
+            type="button"
+            onClick={handleDrawer}
+            className={cn("admin-navigation__burger-menu", {
+              "admin-navigation__burger-menu_is-drawer-open": isDrawerOpen,
+            })}
+          >
+            <span />
           </button>
         </div>
-      </sidebar>
-    </div>
+        <div className="admin-navigation__sidebar">
+          <div className="admin-navigation__sidebar-wrapper">
+            <Link href="/admin" passHref>
+              <a className="admin-navigation__logo" href="replace">
+                АВТОЕЛЕКТРИК <br /> <span className="admin-navigation__logo_green">VAG GROUP</span>
+              </a>
+            </Link>
+            <ul>
+              <li
+                className={cn("admin-navigation__link-item", {
+                  "admin-navigation__link-item_active": asPath === "/admin",
+                })}
+              >
+                <Link href="/admin" passHref>
+                  <a className="admin-navigation__link" href="replace">
+                    <HomeIcon />
+                    Головна
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={cn("admin-navigation__link-item", {
+                  "admin-navigation__link-item_active": asPath === "/admin",
+                })}
+              >
+                <Link href="/admin" passHref>
+                  <a className="admin-navigation__link" href="replace">
+                    <AddIcon />
+                    Записати
+                    <span>2</span>
+                  </a>
+                </Link>
+              </li>
+              <li
+                className={cn("admin-navigation__link-item", {
+                  "admin-navigation__link-item_active": asPath === "/admin",
+                })}
+              >
+                <Link href="/admin" passHref>
+                  <a className="admin-navigation__link" href="replace">
+                    <ClientsIcon />
+                    Клієнти
+                  </a>
+                </Link>
+              </li>
+            </ul>
+            <button type="button" className="admin-navigation__exit">
+              <ExitIcon />
+              Вийти
+            </button>
+          </div>
+        </div>
+      </nav>
+      <AdminDrawer isOpen={isDrawerOpen} onClose={handleDrawerClose} />
+    </>
   );
 };
 
